5e40c2a6f77b643a1a9402d6da4a22fa5b602c67
Akshit-112 <123979642+Akshit-112@users.noreply.github.com>
2024-03-11 20:48:15 -0400

    Assignment 6

--- U_Eri_MyFavouriteCities/src/app/content-list/content-list.component.ts
+++ U_Eri_MyFavouriteCities/src/app/content-list/content-list.component.ts
@@ -5,6 +5,8 @@ import { ContentCardComponent } from '../content-card/content-card.component';
 import { TypefilterPipe } from '../typefilter.pipe';
 import { FormsModule } from '@angular/forms';
 import { HoverAffectDirective } from '../hover-affect.directive';
+import { CityService } from '../city.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-content-list',
@@ -17,7 +19,6 @@ export class ContentListComponent implements OnInit {
   DisplayContentInformation(contentItem: Content) {
     console.log(`ID: ${contentItem.id} and Title: ${contentItem.title}`);
     }
-  @Input () contentItems: Content[] = [];
 
   searchTitle: string = '';
   contentExists: boolean = false;
@@ -30,73 +31,18 @@ export class ContentListComponent implements OnInit {
     this.message = foundItem ? 'Content item exists.' : 'Content item does not exist.';
     this.selectedTitle = foundItem ? foundItem.title : null;
   }
+  selectedContent?: Content;
+    @Input() contentItems: Content[] = [];
+    @Input() items:Content[] = [];
 
-  ngOnInit(): void {
-    this.contentItems =[
-      {
-        id: 0,
-        title: "Ahmedabad",
-        description:"Heritage City located in Gujarat India.",
-        creator:"Ahemad Shah",
-        imgURL:"",
-        type:"Urban",
-        tags: ["Heritage", "metropolian"]
-      },
-      {
-        id: 1,
-        title: "Nadiad",
-        description:"Nadiad is a city in the state of Gujarat, India and the administrative centre of the Kheda district.",
-        creator:"Sardar Vallabhbhai Patel",
-        imgURL:"https://d27p8o2qkwv41j.cloudfront.net/wp-content/uploads/2017/01/Surat-Gujarat1-e1484823578754.jpg",
-        type:"Rural",
-        tags: ["MyHome", "RuralArea"]
-      },
-      {
-        id: 2,
-        title: "Windsor",
-        description:"Small and peaceful city of Ontario Canada.",
-        creator:"Antoine Laumet de Lamothe Cadillac",
-        imgURL:"https://www.sonomacounty.com/sites/default/files/inline-images/christmas_grove_windsor_town_green_900x600.jpg",
-        type:"Rural",
-        tags: ["Currently", "Living"]
-      },
-      {
-        id: 3,
-        title: "New York",
-        description: "Huge developed city in USA",
-        creator: "USA",
-        imgURL: "https://s1.1zoom.me/big0/822/USA_Houses_New_York_City_460967.jpg",
-        type: "Urban",
-        tags: ["USA", "Luxury"]
-      },
-      {
-        id: 4,
-        title: "Calgary",
-        description: "Lovely city located in Albera, canada.",
-        creator: "Urban",
-        imgURL: "https://www.admtl.com/sites/default/files/2020/Calgary-800X534.jpg",
-        type: "Mountains",
-        tags: ["Snow", "Mountain"]
-      },
-      {
-        id: 5,
-        title: "Chicago",
-        description: "The City of Chicago covers an area of 60,000 hectares and sits 176 meters (578 feet) above sea level on the southwestern shore of Lake Michigan",
-        creator: "USA",
-        imgURL: "https://www.chicago.gov/content/dam/city/about/skyline_night.jpg",
-        type: "",
-        tags: ["chiacago", "city"]
-      },      {
-        id: 6,
-        title: "Detroit",
-        description: "Lovely city located in Albera, canada.",
-        creator: "Canada",
-        imgURL: "https://horizonleague.org/images/2018/1/5/Detroit_Skyline.jpg?width=1024&height=576&mode=crop",
-        type: "Rural",
-        tags: ["border", "usa"]
-      }
-      
-    ];
-  }
- 
+    constructor(private CityService:CityService, private messageService: MessageService){ }
+    ngOnInit(): void {
+      this.CityService.getContentObs().subscribe(content => this.contentItems = content);
+      this.CityService.getContentById(4).subscribe(content=> this.items = content);
+    }
+    onSelect(content: Content):void{
+      this.selectedContent = content;
+      this.messageService.add(`Content itme at ${content.id}`);
+      console.log("clicked");
+    }
 }
---
+++ U_Eri_MyFavouriteCities/src/app/helper-files/contentDb.ts
@@ -0,0 +1,65 @@
+export const CONTENT = 
+    [
+        {
+            id: 0,
+            title: "Ahmedabad",
+            description:"Heritage City located in Gujarat India.",
+            creator:"Ahemad Shah",
+            imgURL:"",
+            type:"Urban",
+            tags: ["Heritage", "metropolian"]
+          },
+          {
+            id: 1,
+            title: "Nadiad",
+            description:"Nadiad is a city in the state of Gujarat, India and the administrative centre of the Kheda district.",
+            creator:"Sardar Vallabhbhai Patel",
+            imgURL:"https://d27p8o2qkwv41j.cloudfront.net/wp-content/uploads/2017/01/Surat-Gujarat1-e1484823578754.jpg",
+            type:"Rural",
+            tags: ["MyHome", "RuralArea"]
+          },
+          {
+            id: 2,
+            title: "Windsor",
+            description:"Small and peaceful city of Ontario Canada.",
+            creator:"Antoine Laumet de Lamothe Cadillac",
+            imgURL:"https://www.sonomacounty.com/sites/default/files/inline-images/christmas_grove_windsor_town_green_900x600.jpg",
+            type:"Rural",
+            tags: ["Currently", "Living"]
+          },
+          {
+            id: 3,
+            title: "New York",
+            description: "Huge developed city in USA",
+            creator: "USA",
+            imgURL: "https://s1.1zoom.me/big0/822/USA_Houses_New_York_City_460967.jpg",
+            type: "Urban",
+            tags: ["USA", "Luxury"]
+          },
+          {
+            id: 4,
+            title: "Calgary",
+            description: "Lovely city located in Albera, canada.",
+            creator: "Urban",
+            imgURL: "https://www.admtl.com/sites/default/files/2020/Calgary-800X534.jpg",
+            type: "Mountains",
+            tags: ["Snow", "Mountain"]
+          },
+          {
+            id: 5,
+            title: "Chicago",
+            description: "The City of Chicago covers an area of 60,000 hectares and sits 176 meters (578 feet) above sea level on the southwestern shore of Lake Michigan",
+            creator: "USA",
+            imgURL: "https://www.chicago.gov/content/dam/city/about/skyline_night.jpg",
+            type: "",
+            tags: ["chiacago", "city"]
+          },      {
+            id: 6,
+            title: "Detroit",
+            description: "Lovely city located in Albera, canada.",
+            creator: "Canada",
+            imgURL: "https://horizonleague.org/images/2018/1/5/Detroit_Skyline.jpg?width=1024&height=576&mode=crop",
+            type: "Rural",
+            tags: ["border", "usa"]
+          } 
+    ]
